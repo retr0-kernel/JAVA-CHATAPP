5da19955876ac3b0c956751808744fdac73be1e4
Krish Srivastava <krish22092003@gmail.com>
2023-05-07 17:13:07 +0530

    BufferedReader and PrintWriter

--- Client.java
+++ Client.java
@@ -1,3 +1,5 @@
 public class Client {
-    
+    public static void main(String[] args) {
+        System.out.println("This is the client.....Going to start");
+    }
 }
--- Server.java
+++ Server.java
@@ -1,3 +1,42 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
     
+    ServerSocket server;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+
+
+    public Server(){
+        try {
+            server=new ServerSocket(7777);
+            System.out.println("Server is ready to accept connections....");
+            System.out.println("Waiting...");
+            socket=server.accept(); 
+
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+
+            startReading();
+            startWriting();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startReading(){
+
+    }
+
+    public void startWriting(){
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("This is ther server.....Going to start");
+        new Server();
+    }
 }
