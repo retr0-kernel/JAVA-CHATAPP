c5e5162c0e67f6ec17e718b9e0f2fbd7c5312fea
Krish Srivastava <krish22092003@gmail.com>
2023-05-08 16:38:54 +0530

    bleh

--- Client.java
+++ Client.java
@@ -1,4 +1,16 @@
 public class Client {
+    
+    Socket socket;
+    
+    public Client(){
+    
+        try {
+            socket=new Socket("127.0.0.1",7777);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         System.out.println("This is the client.....Going to start");
     }
