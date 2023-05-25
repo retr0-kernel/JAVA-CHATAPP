fe638c3a071e98bcb31b260d5e5c479d91b843cb
Krish Srivastava <krish22092003@gmail.com>
2023-05-25 22:50:43 +0530

    suka blyat

--- ChatApp.java
+++ ChatApp.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 
 
+
 public class ChatApp extends javax.swing.JFrame 
 {
     public static String cstatus = "Disconnected";
@@ -705,11 +706,6 @@ void seticon()
 
     
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
